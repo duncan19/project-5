05a96708557952a92a4a7a116360889057488a1a
Duncan Robbins <duncanrobbins19@gmail.com>
2019-11-15 10:41:50 -0800

    stop debugging and revert back to previous state

--- _tests_/ages.test.js
+++ _tests_/ages.test.js
@@ -1,9 +1,15 @@
 import { yearsToMercury } from './../src/backend.js';
 
+
 describe('ageschanges', () => {
 
+
+
   test('should correctly changes users age relative to their age in mercury', () => {
-    let newAge = yearsToMurcury(4);
+    const newAge = yearsToMurcury(4);
+
+
+
     expect(usersAgeInMercuryYears).toEqual(16.64);
 
   });
